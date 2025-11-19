ca222f650b3c9c57c0a51de90126035f23ab2457
yashu451 <yashaswini.syashu831@gmail.com>
2025-11-19 11:16:30 +0530

    update

---
+++ tabview/app/_layout.jsx
@@ -0,0 +1,31 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="project"
+        options={{
+          title: 'project',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="th-large" color={color} />,
+        }}
+        />
+      <Tabs.Screen
+        name="contact"
+        options={{
+          title: 'contact',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="phone" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
+
