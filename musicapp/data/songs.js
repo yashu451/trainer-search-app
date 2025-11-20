db79b0371d02123d38f4c4b2c41175f9873a17fc
yashu451 <yashaswini.syashu831@gmail.com>
2025-11-20 19:38:23 +0530

    update

--- musicapp/data/songs.js
+++ musicapp/data/songs.js
@@ -5,20 +5,20 @@ export const songs = [
     title: "Kesariya",
     artist: "Arijit Singh",
     image: { uri: "https://ik.imagekit.io/rrvxomyh1d/kes.jpeg" },
-    audio: { uri: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" }
+    audio: { uri: "https://ik.imagekit.io/rrvxomyh1d/kesr.mp3" }
   },
   {
     id: 2,
     title: "Preetiya hesare ninu",
     artist: "Raghu Dixit",
     image: { uri: "https://ik.imagekit.io/rrvxomyh1d/pree.jpeg" },
-    audio: { uri: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3" }
+    audio: { uri: "https://ik.imagekit.io/rrvxomyh1d/prret.mp3" }
   },
   {
     id: 3,
     title: "Oorum blood",
     artist: "Sai abayankar",
     image: { uri: "https://ik.imagekit.io/rrvxomyh1d/oo.jpeg" },
-    audio: { uri: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3" }
+    audio: { uri: "https://ik.imagekit.io/rrvxomyh1d/oor.mp3" }
   }
 ];
